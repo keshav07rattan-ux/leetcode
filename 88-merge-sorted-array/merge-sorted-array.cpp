b0dd02cac3212c57538abce4f12d78d9e7a79ff0
keshav07rattan-ux <keshav07rattan@gmail.com>
2026-09-09 09:48:39 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (20.32%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+
+        int i = m - 1;       // last real element of nums1
+        int j = n - 1;       // last element of nums2
+        int k = m + n - 1;   // last position of nums1
+
+        while (j >= 0) {
+
+            if (i >= 0 && nums1[i] > nums2[j]) {
+
+                nums1[k] = nums1[i];
+
+                i--;
+            }
+            else {
+
+                nums1[k] = nums2[j];
+
+                j--;
+            }
+
+            k--;
+        }
+    }
+};
